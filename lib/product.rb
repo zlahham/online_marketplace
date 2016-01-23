a14945093227bbe8bd56942b559779a4d21bf225
Zaid Al Lahham <zlahham@gmail.com>
2016-01-23 19:08:56 +0000

    product has code

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,10 @@
 module EPOS
   class Product
-    attr_reader :name
+    attr_reader :name, :code
 
     def initialize(options = {})
       @name = options.fetch(:name)
+      @code = options.fetch(:code)
     end
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,10 +1,14 @@
 module EPOS
   describe Product do
     context '#initialize' do
-      let(:product) { Product.new(name: 'Cat Food') }
+      let(:product) { Product.new(name: 'Cat Food', code: '001') }
 
       it 'shows the product name' do
         expect(product.name).to eq 'Cat Food'
+      end
+
+      it 'shows the product code' do
+        expect(product.code).to eq '001'
       end
     end
   end
