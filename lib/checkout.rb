45ee78014c6bb3f344b834355db7b24bfb0e2ecf
Zaid Al Lahham <zlahham@gmail.com>
2016-01-24 16:23:51 +0000

    scan implemented

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,22 @@
 module EPOS
   class Checkout
-    attr_reader :rules, :basket, :total
+    attr_reader   :rules, :basket
+    attr_accessor :total_price
 
     def initialize(rules = PromotionalRules.new, basket = Basket.new)
-      @rules = rules
-      @basket = basket
-      @total = 0
+      @rules       = rules
+      @basket      = basket
+      @total_price = 0
+    end
+
+    def scan(item_code)
+      basket.add(item_code)
+      @total_price += basket.content.last.price
+    end
+
+    def total
+      # rules.spending_over_sixty(total)
+      # rules.lavender_hearts_promotion(basket, item_code)
     end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,6 +1,6 @@
 module EPOS
   describe Checkout do
-    context '#initialize' do
+    context ',#initialize' do
       it 'has the PromotionalRules ready' do
         expect(subject.rules).to be_instance_of(PromotionalRules)
       end
@@ -10,7 +10,23 @@ module EPOS
       end
 
       it 'begins with a total of zero' do
-        expect(subject.total).to eq 0
+        expect(subject.total_price).to eq 0
+      end
+    end
+
+    context '#scan' do
+      it 'allows us to add the item to the checkout basket' do
+        subject.scan('001')
+        expect(subject.basket.content.first.code).to eq '001'
+      end
+    end
+
+    context '#total' do
+      xit 'returns the total cost with regards to promotional_rules' do
+        subject.scan('001')
+        subject.scan('001')
+        subject.scan('001')
+        expect(subject.total).to eq 25.5
       end
     end
   end
