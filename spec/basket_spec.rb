25488aba34053eacadd9a58249b9b84eae363e00
Zaid Al Lahham <zlahham@gmail.com>
2016-01-24 13:33:01 +0000

    number of items passing

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -2,6 +2,7 @@ module EPOS
   describe Basket do
     let(:products)       { Basket::PRODUCTS }
     let(:product_code_1) { products.keys.first }
+    let(:product_code_3) { products.keys.last }
 
     context '#initialize' do
       it 'contains no products when' do
@@ -43,6 +44,14 @@ module EPOS
       it 'returns the number of items in a basket' do
         subject.add(product_code_1)
         expect(subject.total_items).to eq 1
+      end
+    end
+
+    context '#number_of_items' do
+      it 'return the number of items of a specif product' do
+        subject.add(product_code_1)
+        subject.add(product_code_3)
+        expect(subject.number_of_items(product_code_1)).to eq 1
       end
     end
   end
